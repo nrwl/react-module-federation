a4bc32cad346f397cebe7ab99b57e7df7cf0b4f6
Nicholas Cunningham <ndcunningham@gmail.com>
2022-05-05 10:40:32 -0600

    test PR

--- libs/about-main/src/lib/about-main.tsx
+++ libs/about-main/src/lib/about-main.tsx
@@ -1,12 +1,7 @@
 import { Greeting } from '@react-module-federation/shared-components';
 
-/* eslint-disable-next-line */
-export interface AboutMainProps {}
-
-export function AboutMain(props: AboutMainProps) {
-  return (
-    <Greeting title='about'/>
-  );
+export function AboutMain() {
+  return <Greeting title="about" />;
 }
 
 export default AboutMain;

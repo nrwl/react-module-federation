3dcbe8d844c272c896e5b26c9adbfcde85fb0ebb
Nicholas Cunningham <ndcunningham@gmail.com>
2022-12-21 09:16:58 -0700

    Update to latest version

--- apps/about/jest.config.ts
+++ apps/about/jest.config.ts
@@ -1,9 +1,10 @@
-module.exports = {
+/* eslint-disable */
+export default {
   displayName: 'about',
-  preset: '../../jest.preset.ts',
+  preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
-    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/react/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/about',

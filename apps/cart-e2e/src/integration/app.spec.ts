1d6be445a0e152e843bd28264e566883ae8c131a
Yevhen Zhylin <eugene.zhylin@mckesson.com>
2023-09-18 22:36:27 +0300

    setup

--- apps/cart-e2e/src/integration/app.spec.ts
+++
@@ -1,9 +0,0 @@
-import { getGreeting } from '../support/app.po';
-
-describe('cart', () => {
-  beforeEach(() => cy.visit('/cart'));
-
-  it('should display welcome message', () => {
-    getGreeting().contains('Welcome cart');
-  });
-});
--- apps/host-e2e/src/integration/app.spec.ts
+++
@@ -1,9 +0,0 @@
-import { getGreeting } from '../support/app.po';
-
-describe('host', () => {
-  beforeEach(() => cy.visit('/'));
-
-  it('should display welcome message', () => {
-    getGreeting().contains('Welcome shop');
-  });
-});

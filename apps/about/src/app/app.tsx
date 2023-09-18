3e0f5ad65fd2d2649f8543325c3d0a52c45cf098
Yevhen Zhylin <eugene.zhylin@mckesson.com>
2023-09-18 23:10:38 +0300

    fix: typo in the heading

--- apps/about/src/app/app.tsx
+++ apps/about/src/app/app.tsx
@@ -5,7 +5,7 @@ export function App() {
   return (
     <>
       <AboutMain/>
-      <div>Hello from the about app</div>
+      <div>Hello from the about app!</div>
       <p>Lorem</p>
     </>
   );

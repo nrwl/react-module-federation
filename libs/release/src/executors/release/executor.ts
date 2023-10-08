cf21b663be5d013a19636ee38e2bcada5db85b28
Yevhen Zhylin <141925960+yzhylin@users.noreply.github.com>
2023-10-08 20:47:25 +0300

    fix(ci): commit and push (#11)

--- libs/release/src/executors/release/executor.ts
+++ libs/release/src/executors/release/executor.ts
@@ -1,7 +1,27 @@
+import { writeFileSync } from 'fs'
 import { ReleaseExecutorSchema } from './schema';
+import { ExecutorContext } from '@nx/devkit';
 
-export default async function runExecutor(options: ReleaseExecutorSchema) {
-  console.log('Executor ran for Release', options);
+import { exec } from 'child_process';
+import { promisify } from 'util';
+const execa = promisify(exec);
+
+const file = 'CHANGELOG.md'
+
+export default async function runExecutor(options: ReleaseExecutorSchema, context: ExecutorContext) {
+  const { projectName, workspace, root} = context;
+
+  for (const name in  workspace.projects) {
+    if (projectName === name) {
+      const path = `${root}/${workspace.projects[name].root}`;
+
+      writeFileSync(`${path}/${file}`, 'changelog \n', { flag: 'a'})
+    }
+  }
+
+  await execa(`git add . && git commit -m 'chore(release): 1.35.1 [skip ci]'`)
+  await execa(`git push`)
+
   return {
     success: true,
   };
